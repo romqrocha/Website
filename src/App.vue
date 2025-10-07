ce5790b44da4d752b42597fa92a56be7870120e7
Rodrigo <91029590+romqrocha@users.noreply.github.com>
2025-10-06 21:13:18 -0700

    basic blog post structure done

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,17 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
+import BlogPost from './components/BlogPost.vue'
 </script>
 
 <template>
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  <BlogPost
+    title="How to make your own dev blog using Vue.js"
+    postURL="./post"
+    date="2025-10-06"
+    v-bind:tags="['Webdev', 'Tutorial']"
+    subtitle="Get your knowledge out there"
+  />
 </template>
 
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
+
 </style>
---
+++ src/components/BlogPost.vue
@@ -0,0 +1,47 @@
+<script setup lang="ts">
+
+interface Props {
+    title: string
+    postURL: string
+    date: string
+    tags: string[]
+    subtitle: string
+    thumbnailURL?: string
+}
+
+const props = defineProps<Props>()
+
+</script>
+
+<template>
+    <div>
+        <img src=""/>
+
+        <a :href="props.postURL" :title="props.title">
+            <h2 class="text-3xl font-bold underline">
+                {{ props.title }}
+            </h2>
+        </a>
+        
+
+        <time datetime="">
+            {{ props.date }}
+        </time>
+
+        <ul v-for="(tag, index) in props.tags" :key="index">
+            <li>
+                <span class="bg-green-100 animate-spin">
+                    {{ tag }}
+                </span>
+            </li>
+        </ul>
+
+        <p>
+            {{ props.subtitle }}
+        </p>
+    </div>
+</template>
+
+<style scoped>
+
+</style>
